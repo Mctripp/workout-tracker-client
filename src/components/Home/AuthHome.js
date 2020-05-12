0bed9254a3c1c8f60adf13c804a635680416a71f
Mctripp <mctripp94@gmail.com>
2020-05-12 12:34:05 -0400

    Setup workout accordions
    Set up the display accordions for users' workouts

--- src/components/Home/AuthHome.js
+++ src/components/Home/AuthHome.js
@@ -1,36 +1,46 @@
 import React, { Fragment } from 'react'
-import { Col, Row, Accordion, Button } from 'react-bootstrap'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Accordion from 'react-bootstrap/Accordion'
 import AccordionCard from './../shared/AccordionCard'
+import CreateNewWorkout from './../CreateWorkout/CreateWorkoutButton'
 
-const AuthHome = ({ user }) => (
-  <Fragment>
-    The user is signed in!
-    <Row>
-      <Button className='btn-primary'>Create new workout</Button>
-    </Row>
-    <Row>
-      <Col>
-        Upcoming workouts will go here
-        <Accordion>
-          <AccordionCard
-            key='1'
-            headerContent='Example upcoming workout'
-            bodyContent='Example stats, description'
-          />
-        </Accordion>
-      </Col>
-      <Col>
-        Completed workouts will go here
-        <Accordion>
-          <AccordionCard
-            key='1'
-            headerContent='Example completed workout'
-            bodyContent='Example stats, description'
-          />
-        </Accordion>
-      </Col>
-    </Row>
-  </Fragment>
-)
+const AuthHome = ({ user }) => {
+  return (
+    <Fragment>
+      The user is signed in!
+      <Row>
+        <CreateNewWorkout/>
+      </Row>
+      <Row>
+        <Col>
+          Upcoming workouts will go here
+          <Accordion>
+            <AccordionCard
+              eventKey="1"
+              headerContent='Example upcoming workout 1'
+              bodyContent='Example stats, description'
+            />
+            <AccordionCard
+              eventKey="2"
+              headerContent='Example upcoming workout 2'
+              bodyContent='Example stats, description'
+            />
+          </Accordion>
+        </Col>
+        <Col>
+          Completed workouts will go here
+          <Accordion>
+            <AccordionCard
+              key="1"
+              headerContent='Example completed workout 1'
+              bodyContent='Example stats, description'
+            />
+          </Accordion>
+        </Col>
+      </Row>
+    </Fragment>
+  )
+}
 
 export default AuthHome
--- src/components/shared/AccordionCard.js
+++ src/components/shared/AccordionCard.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Accordion, Card, Button } from 'react-bootstrap'
 
-const AccordionCard = ({ key, headerContent, bodyContent }) => (
+const AccordionCard = ({ eventKey, headerContent, bodyContent }) => (
   <Card>
     <Card.Header>
-      <Accordion.Toggle as={Button} variant="link" eventKey={`${key}`}>
+      <Accordion.Toggle as={Button} variant="link" eventKey={`${eventKey}`}>
         {headerContent}
       </Accordion.Toggle>
     </Card.Header>
-    <Accordion.Collapse eventKey={`${key}`}>
+    <Accordion.Collapse eventKey={`${eventKey}`}>
       <Card.Body>{bodyContent}</Card.Body>
     </Accordion.Collapse>
   </Card>

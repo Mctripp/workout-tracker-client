84fb437ca2643135135f296dd1f47c005412a23b
Mctripp <mctripp94@gmail.com>
2020-05-14 15:54:32 -0400

    Formatted viewWorkouts page

--- src/components/ViewWorkouts/ViewWorkouts.js
+++ src/components/ViewWorkouts/ViewWorkouts.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
 import axios from 'axios'
 import apiUrl from './../../apiConfig'
 import moment from 'moment'
@@ -26,18 +27,24 @@ const ViewWorkouts = ({ user }) => {
   }, [])
 
   const workoutsJsx = workouts.map(workout => (
-    <li key={workout._id}>
-      <Link to={`/workouts/${workout._id}`}>{workout.name}: {moment(workout.date_time).format('LLLL')}</Link>
-    </li>
+    <Card key={workout._id}>
+      <Card.Body>
+        <Card.Title>
+          <Link to={`/workouts/${workout._id}`}>{workout.name}</Link>
+        </Card.Title>
+        <Card.Text>
+          {workout.description.substring(0, 20)}...<br></br>
+          <b>When: </b>{moment(workout.date_time).format('LLLL')}
+        </Card.Text>
+      </Card.Body>
+    </Card>
   ))
 
   return (
     // change this to accordion
     <Container fluid>
-      <h1>All workouts by date (descending)</h1>
-      <ul>
-        {workoutsJsx}
-      </ul>
+      <h2>All workouts by date (descending)</h2>
+      {workoutsJsx}
     </Container>
   )
 }
